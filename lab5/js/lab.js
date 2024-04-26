5ac4fb350ea92e6db17f941b927ce116adc1bf0e
krispycrimes <krisgburiel@gmail.com>
2024-04-25 23:39:12 -0700

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,19 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: kristine buriel
+// Date: 4/25/24
 
-// Constants
+// Define Variables 
+make = "toyota";
+model = "tacoma"; 
+color = "silver";
+year = 2019;
 
-// Functions
+// caluclate
+age= 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output 
+document.writeln("make:" + make + "<br>");
+document.writeln("model:" + model + "<br>");
+document.writeln("color" + color + "<br>");
+document.writeln("year:" + year + "<br>");
+document.writeln("age:" + age + "<br>");
