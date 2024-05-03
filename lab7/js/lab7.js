d3457af9fe5cc4a404b90eb9b6b1cba8c708096b
krispycrimes <krisgburiel@gmail.com>
2024-05-02 18:23:53 -0700

    trying to make a new lab file

---
+++ lab7/js/lab7.js
@@ -0,0 +1,23 @@
+// index.js - lab 5! cool!
+// Author: kristine buriel
+// Date: 4/25/24
+
+// Define Variables 
+make = "toyota";
+model = "tacoma"; 
+color = "silver";
+year = 2019;
+
+// Own true or false
+ownIt = false;
+
+
+// caluclate
+age= 2024 - year;
+
+// output 
+document.writeln("make:" + make + "<br>");
+document.writeln("model:" + model + "<br>");
+document.writeln("color" + color + "<br>");
+document.writeln("year:" + year + "<br>");
+document.writeln("age:" + age + "<br>");
