601967f72cccf732b0a80eb5da07253646059501
krispycrimes <krisgburiel@gmail.com>
2024-05-02 22:03:11 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -2,12 +2,25 @@
 // Author: kristine buriel
 // Date: 5/02/24
 
-//main function, this is all the dialogue 
-
+//let's sort the characters from the user input 
+function sortUserName () { var userName = window.prompt("aaAHH!. Please tell me your name so I can make a battle cry!");
+console.log("UserName =", UserName);
+// split string to array 
+var nameArray = UserName.split('');
+console.log("nameArray =", nameArray);
+// sort the array 
+var nameArraySort = nameArraySort;
+// join array back to a string
+var nameSorted = nameArraySort.join('');
+console.log ("nameSorted =", nameSorted);
+// I like to see these as steps
+return nameSorted;
+// these do the stuff and tell the user that it's doing it
+}
 function main() {
-    var userName = window.prompt("aaAHH!. Please tell me your name so I can make a battle cry!");
-    document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
-    var sortedName = sortUserName(userName);
+    var UserName = window.prompt("aaAHH!. Please tell me your name so I can make a battle cry!");
+    document.writeln("<style>:root {--textlen: " + UserName.length + ";}</style>");
+    var sortedName = sortUserName(UserName);
     document.writeln("AAAAAHHHHHHHH! FIGHT ON, " + sortedName + "</br></br>");
     var randomName = randomizeName(userName);
     document.writeln("Shouting your name now<br>AHHHHHHHHHH!");
