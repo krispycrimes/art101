409eaf46b481c0603917fbe8858ebdec082cc9de
krispycrimes <krisgburiel@gmail.com>
2024-05-02 22:28:23 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -5,21 +5,33 @@ function sortUserName() {
     // split string to array 
     var nameArray = userName.split('');
     console.log("nameArray =", nameArray);
-    // sort the array 
     // missed to call .sort method on array. Fixed this line as below.
     var nameArraySort = 	nameArray.sort();
     // join array back to a string
     var nameSorted = 	nameArraySort.join('');
 
     console.log ("nameSorted =", nameSorted);
-    // I like to see these as steps
     return nameSorted;
 }
 function main() {
     var userName = window.prompt("aaAHH!. Please tell me your username so I can make a battle cry!");
     document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
-    var sortedName = 	sortUserName(userName);
+    var sortedName= 	sortUserName(userName);
 
-    document.writeln("AAAAAHHHHHH! FIGHT ON, " + sortedName + "</br></br>");
+    document.writeln("AAAAAHHHHFIGHT ON, " + sortedName + "</br></br>");
+    // Call randomizeName function
+    var randomName=	randomizeNames(userName);	
+    document.writeln("<div class='name'>" + randomName+ "</div>");
+}
+function randomizeNames(name){
+    var shuffledString="";
+    for (var i=name.length-1;i>0;i--)
+    {
+        var j=Math.floor(Math.random() * (i+1));
+        shuffledString+=name[j];
+        [name[i],name[j]]=[name[j],name[i]];
+    }
+    shuffledString+=names[0];
     
+    return shuffledString;
 }
