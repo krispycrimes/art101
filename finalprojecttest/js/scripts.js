1ab802129859c40213d67cb29bea362e4c40bbfa
krispycrimes <krisgburiel@gmail.com>
2024-06-07 06:04:20 -0700

    updated the three for a visible story

--- finalprojecttest/js/scripts.js
+++ finalprojecttest/js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $('#madlibs-form').submit(function(event) {
         event.preventDefault();
-        
+
         var noun = $('#noun').val();
         var verb = $('#verb').val();
         var adjective = $('#adjective').val();
-        
+
         var story = `Once upon a time, there was a ${adjective} ${noun} that loved to ${verb}.`;
-        
+
         $('#result').text(story);
     });
 });
