f22dd804b8ec4be2bcec8c3d87787b51259e51c0
krispycrimes <krisgburiel@gmail.com>
2024-06-09 13:16:59 -0700

    crystal ball event function

--- lab15/js/lab15.js
+++ lab15/js/lab15.js
@@ -4,19 +4,19 @@
 
 const URL = "https://yesno.wtf/api";
 
-$("#crystalBall").click(function() {
-  let question = $("#input").val();
-  console.log(question);
-  $("#the-question").html("<p>Your Question: <span class='bold'>" + question + "</span></p>");
+$(document).ready(function() {
+  $("#crystalBall").click(function() {
+      let question = $("#input").val();
+      console.log(question);
+      $("#the-question").html("<p>Your Question: <span class='bold'>" + question + "</span></p>");
 
-
-    $.ajax({
-        url: URL,
-        type: "GET"
-    })
-    .done(function(data) {
-    
-        let yN =  data.answer;
-        $("#output").html("<p>Your Answer: <span class='bold'>" + yN + "</span></p>" + "<img class='center' src=" + data.image + ">");    
-    });
-})
+      $.ajax({
+          url: URL,
+          type: "GET"
+      })
+      .done(function(data) {
+          let yN =  data.answer;
+          $("#output").html("<p>Your Answer: <span class='bold'>" + yN + "</span></p>" + "<img class='center' src=" + data.image + ">");    
+      });
+  });
+});
