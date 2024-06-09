685c8041c899ce83b1124560eb0c85872356a3a8
krispycrimes <krisgburiel@gmail.com>
2024-06-09 13:05:12 -0700

    turned it to yes or no

--- lab15/js/lab15.js
+++ lab15/js/lab15.js
@@ -1,26 +1,21 @@
-// Using the core $.ajax() method
-$.ajax({
-  // The URL for the request (from the api docs)
-  url: "https://yerkee.com/api/fortune",
-  // The data to send (will be converted to a query string)
-  data: { 
-    // here is where any data required by the api 
-    // goes (check the api docs)
-    format: "json"
-  },
-  // Whether this is a POST or GET request
-  type: "GET",
-  // The type of data we expect back
-  dataType : "json",
-  // What do we do when the api call is successful
-  // all the action goes in here
-  success: function(data) {
-    // do stuff
-    console.log(data);
-  },
-  // What we do if the api call fails
-  error: function (jqXHR, textStatus, errorThrown) { 
-    // do stuff
-    console.log("Error:", textStatus, errorThrown);
-  }
+// index.js - AJAX
+// Author: Kristibe b
+// Date: 6/9/24
+
+const URL = "https://yesno.wtf/api";
+
+$("#activate").click(function(){
+    let question = $("#input").val();
+    console.log(question);
+    $("#the-question").html("<p>Your Question: <span class='bold'>" + question + "</span></p>")
+
+    $.ajax({
+        url: URL,
+        type: "GET"
+    })
+    .done(function(data) {
+    
+        let yN =  data.answer;
+        $("#output").html("<p>Your Answer: <span class='bold'>" + yN + "</span></p>" + "<img class='center' src=" + data.image + ">");    
+    });
 })
