e9ea22ab55a53d9f186d6d0c8624de3608568214
krispycrimes <krisgburiel@gmail.com>
2024-06-09 17:48:07 -0700

    oh mygod

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -2,15 +2,16 @@
 // Author: Kristine b
 // Date: 6/9/24
 
-const URL = "https://xkcd.com/info.0.json";
+const URL = "comic.json"; 
 
 $(document).ready(function() {
+    console.log("JS file loaded"); 
     $.ajax({
         url: URL,
         type: "GET",
         dataType: "json",
         success: function(comicObj) {
-            console.log(comicObj);
+            console.log(comicObj); 
             $("#output").html(`
                 <h2>${comicObj.title}</h2>
                 <img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}">
