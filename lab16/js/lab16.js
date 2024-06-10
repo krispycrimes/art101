4f632cabe7a86089b3eb04c50c7bea3d4f8173ea
krispycrimes <krisgburiel@gmail.com>
2024-06-09 17:30:59 -0700

    fix meeee

---
+++ lab16/js/lab16.js
@@ -0,0 +1,22 @@
+// index.js - AJAX
+// Author: Kristibe b
+// Date: 6/9/24
+
+const URL = "https://yesno.wtf/api";
+
+$(document).ready(function() {
+  $("#crystalBall").click(function() {
+      let question = $("#input").val();
+      console.log(question);
+      $("#the-question").html("<p>Your Question: <span class='bold'>" + question + "</span></p>");
+
+      $.ajax({
+          url: URL,
+          type: "GET"
+      })
+      .done(function(data) {
+          let yN =  data.answer;
+          $("#output").html("<p>Your Answer: <span class='bold'>" + yN + "</span></p>" + "<img class='center' src=" + data.image + ">");    
+      });
+  });
+});
